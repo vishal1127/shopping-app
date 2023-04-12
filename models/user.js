824d6f576c245126351f692f6e60c855fd27dd9c
vishal2796 <vishal.pb.27@gmail.com>
2023-04-12 16:10:28 +0530

    added orders functionality

--- models/user.js
+++ models/user.js
@@ -80,6 +80,37 @@ class User {
       );
   }
 
+  addOrder() {
+    const db = getDb();
+    return this.getCart()
+      .then((products) => {
+        const order = {
+          items: products,
+          user: {
+            _id: new ObjectId(this._id),
+            name: this.name,
+            email: this.email,
+          },
+        };
+        return db.collection("orders").insertOne(order);
+      })
+      .then((result) => {
+        this.cart = { items: [] }; //empty the user object
+        return db.collection("users").updateOne(
+          { _id: new ObjectId(this._id) },
+          { $set: { cart: { items: [] } } } //empty cart in database
+        );
+      });
+  }
+
+  getOrders() {
+    const db = getDb();
+    return db
+      .collection("orders")
+      .find({ "user._id": new ObjectId(this._id) })
+      .toArray();
+  }
+
   static findById(userId) {
     const db = getDb();
     return db
